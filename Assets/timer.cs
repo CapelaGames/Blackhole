cedb8f69d43f6375756d3d128ef9ed004aaa97cc
DESKTOP-MRT5ACV\tharm <tharmaglee@gmail.com>
2018-01-28 15:55:34 +1100

    timer

---
+++ Assets/timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    public GameObject enable;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(enabledisable());	
+        if(enable == null)
+        {
+            Debug.LogError("enable not attached");
+        }
+	}
+	
+
+
+    public IEnumerator enabledisable()
+    {
+         yield return new WaitForSeconds(10f);
+
+        gameObject.SetActive(false);
+        enable.SetActive(true);
+    }
+    
+}
---
+++ Assets/timerenabletwo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class things
+{
+    public GameObject enable;
+    public float time;
+}
+
+public class timerenabletwo : MonoBehaviour
+{
+
+    public List<things> enable;
+
+    private void OnEnable()
+    {
+        StartDoing();
+    }
+
+    private void StartDoing()
+    {
+        foreach (things thing in enable)
+        {
+            StartCoroutine(enabledisable(thing));
+        }
+    }
+
+
+    public IEnumerator enabledisable(things thing)
+    {
+        yield return new WaitForSeconds(thing.time);
+        
+        thing.enable.SetActive(true);
+    }
+}
